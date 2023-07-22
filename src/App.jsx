1ad29dbe13b42a595aa5136dca259aaa45412279
Marylyne <85481945+marylyne-mutindi@users.noreply.github.com>
2023-07-22 10:30:41 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Marylyne Mutindi",
+  title: "Web Designer & UI/UX designer",
+  email: "marylynemutindi@gmailcom",
+  gitHub: "marylyne-mutindi",
+  instagram: "marylyne-mutindi",
+  linkedIn: "marylyne-mutindi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "marylyne-mutindi",
+  youTube: "marylyne-mutindi",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../../__images__/background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a UI/UX student studying at St Pauls University. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
+  "Mobile App Development",
+  "UI/UX designer",
   "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Full Stack Developer",
+  "Graphic designer",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My passion lies in utilizing technology to make a difference. I am driven to create impactful web applications that enhance productivity, solve problems, and bring joy to users. I believe in a user-centered development approach, focusing on intuitive interfaces, smooth interactions, and seamless navigation..";
 
 const About = () => {
   return (
